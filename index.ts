d9e3009af2fb29b44efb248f3a949b80c6911b79
evanrs <me@evanrs.com>
2021-05-24 14:08:57 -0400

    refactor: use unfetch

--- index.ts
+++ index.ts
@@ -1,21 +1,18 @@
+import unfetch from "isomorphic-unfetch";
+
+// TODO use unfetch.IsomorphicResponse?
 export interface TypedResponse<T> extends Response {
   json<P = T>(): Promise<P>;
 }
 
-function __fetch<T>(input: RequestInfo, options?: RequestInit): Promise<TypedResponse<T>> {
-  return fetch(input, options).then(memoize);
+export function fetch<T>(input: RequestInfo, options?: RequestInit): Promise<TypedResponse<T>> {
+  return unfetch(input, options).then(memoize);
 }
 
-// Perhaps an unforgivable sin … but we'll see
+// an unforgivable sin? … perhaps, but we'll see
 function memoize<T>(response: TypedResponse<T>) {
   response.json = () => response.clone().json();
   response.text = () => response.clone().text();
 
   return response;
-}
-
-export { __fetch as fetch };
-
-function isPlainObject(obj: unknown): obj is object {
-  return obj != null && Object.prototype.toString.call(obj) === "[object Object]";
 }
